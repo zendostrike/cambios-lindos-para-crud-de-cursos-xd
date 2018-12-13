ad6e30b63acd36765efad1e73fe6666c08ab6a72
Giancarlo R <gecedeveloper@gmail.com>
2018-12-13 17:40:49 -0500

    fixing syntaxis

--- index.py
+++ index.py
@@ -1,29 +1,29 @@
 cambios = {
-  'cursos': [
-    'No pueden crear curso',
-    'No pueden eliminar curso',
-    'Sobreescribir evaluación en temas (bugfix)',
-    'Todo lo que dice clases es temas, y temas por videos (lo necesario)',
-    'Mover tabla de evaluaciones a edición de curso'
-    'Previsualización de imagen de curso',
-    'Dentro de editar tema, quitar botón agregar URL',
-    'Subir múltiples archivos con drag&drop',
-    'En temas, quitar header title y mover el +'
-  ]
-  'pagos': [
-    'Solo ven pagos aceptados',
-    'En la modal no ven data de pasarela',
-    'No ven pasarela ni método de pago',
-    'Solo ven el precio final',
-    'Para admin, agregar columna de precio final'
-  ],
-  'matriculas': [
-    'Ocultar switch de activo',
-    'Profesores solo ven atrículas activas'
-  ],
-  "layoyt": [
-    "Quitar la barra superior (azul)"
-  ]
+    'cursos': [
+        'No pueden crear curso',
+        'No pueden eliminar curso',
+        'Sobreescribir evaluación en temas (bugfix)',
+        'Todo lo que dice clases es temas, y temas por videos (lo necesario)',
+        'Mover tabla de evaluaciones a edición de curso'
+        'Previsualización de imagen de curso',
+        'Dentro de editar tema, quitar botón agregar URL',
+        'Subir múltiples archivos con drag&drop',
+        'En temas, quitar header title y mover el +'
+    ],
+    'pagos': [
+        'Solo ven pagos aceptados',
+        'En la modal no ven data de pasarela',
+        'No ven pasarela ni método de pago',
+        'Solo ven el precio final',
+        'Para admin, agregar columna de precio final'
+    ],
+    'matriculas': [
+        'Ocultar switch de activo',
+        'Profesores solo ven atrículas activas'
+    ],
+    "layoyt": [
+        "Quitar la barra superior (azul)"
+    ]
 }
 
 print(cambios)
