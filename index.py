f3afba3119642f80eb64ad96e59805830ce2357c
Giancarlo R <gecedeveloper@gmail.com>
2018-12-13 17:22:03 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+cambios = {
+  'cursos': [
+    'No pueden crear curso',
+    'No pueden eliminar curso',
+    'Sobreescribir evaluación en temas (bugfix)',
+    'Todo lo que dice clases es temas, y temas por videos (lo necesario)',
+    'Mover tabla de evaluaciones a edición de curso'
+    'Previsualización de imagen de curso',
+    'Dentro de editar tema, quitar botón agregar URL',
+    'Subir múltiples archivos con drag&drop'
+  ]
+  'pagos': [
+    'Solo ven pagos aceptados',
+    'En la modal no ven data de pasarela',
+    'No ven pasarela ni método de pago',
+    'Solo ven el precio final'
+  ],
+  'matriculas': [
+    'Ocultar switch de activo',
+    'Profesores solo ven atrículas activas'
+  ]
+}
+
+print(cambios)
