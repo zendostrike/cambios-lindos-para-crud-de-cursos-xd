f9922676de858658c4d25a2e0db6eccd2342bf16
Giancarlo R <gecedeveloper@gmail.com>
2018-12-13 17:26:53 -0500

    :)

--- index.py
+++ index.py
@@ -7,17 +7,22 @@ cambios = {
     'Mover tabla de evaluaciones a edición de curso'
     'Previsualización de imagen de curso',
     'Dentro de editar tema, quitar botón agregar URL',
-    'Subir múltiples archivos con drag&drop'
+    'Subir múltiples archivos con drag&drop',
+    'En temas, quitar header title y mover el +'
   ]
   'pagos': [
     'Solo ven pagos aceptados',
     'En la modal no ven data de pasarela',
     'No ven pasarela ni método de pago',
-    'Solo ven el precio final'
+    'Solo ven el precio final',
+    'Para admin, agregar columna de precio final'
   ],
   'matriculas': [
     'Ocultar switch de activo',
     'Profesores solo ven atrículas activas'
+  ],
+  "layoyt": [
+    "Quitar la barra superior (azul)"
   ]
 }
 
